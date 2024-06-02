ab4571be6e7954cdd07fe1c3beef4d1f40dd2bb6
hbl917070 <hbl917070@hotmail.com.tw>
2024-06-02 23:57:50 +0800

    Fix issue where middle mouse button is not working

--- Www/ts/Lib.ts
+++ Www/ts/Lib.ts
@@ -888,8 +888,13 @@ class SelectionManager {
 
     private init() {
         document.addEventListener("mousedown", (event: MouseEvent) => {
+            // 點擊的不是左鍵
+            if (event.button !== 0) {
+                return;
+            }
+
             const target = event.target as Element;
-            // 如果點擊的是文字輸入框，則不阻止選取
+            // 如果點擊的是文字輸入框
             if (target instanceof HTMLInputElement ||
                 target instanceof HTMLTextAreaElement ||
                 target instanceof HTMLSelectElement
