b73e3fd34e4e86f3ff27922890fda966f3cf5787
hbl917070 <hbl917070@hotmail.com.tw>
2026-04-27 04:15:36 +0800

    Move AppInfo category to a separate file

--- Tiefsee/WebWindow.cs
+++ Tiefsee/WebWindow.cs
@@ -270,35 +270,6 @@ public class WebWindow : FormNone {
         return JsonSerializer.Serialize(appInfo);
     }
 
-    public class AppInfo {
-        /// <summary> 命令列參數 </summary>
-        public string[] args { get; set; }
-        /// <summary> 1=直接啟動  2=快速啟動  3=快速啟動且常駐  4=單一執行個體  5=單一執行個體且常駐 </summary>
-        public int startType { get; set; }
-        /// <summary> 程式開始的port </summary>
-        public int startPort { get; set; }
-        /// <summary> 程式所在的資料夾 </summary>
-        public string appDirPath { get; set; }
-        /// <summary> 程式的暫存資料夾 </summary>
-        public string appDataPath { get; set; }
-        /// <summary> 暫存資料夾 - 從網路下載的檔案 </summary>
-        public string tempDirWebFile { get; set; }
-        /// <summary> 目前使用的 port </summary>
-        public int mainPort { get; set; }
-        /// <summary> setting.js 的路徑 </summary>
-        public string settingPath { get; set; }
-        /// <summary> setting.js 的文字 </summary>
-        public string settingTxt { get; set; }
-        /// <summary> 是否為快速預覽的視窗。 0=不是快速預覽 1=長按空白鍵 2=長按滑鼠中鍵 </summary>
-        public int quickLookRunType { get; set; }
-        /// <summary> 是否為商店版 APP </summary>
-        public bool isStoreApp { get; set; }
-        /// <summary> 是否為 win11 </summary>
-        public bool isWin11 { get; set; }
-        /// <summary> 哪些擴充是有啟用的 </summary>
-        public PluginAvailability plugin { get; set; } = PluginRegistry.pluginAvailability;
-    }
-
     /// <summary>
     /// 取得 CoreWebView2Environment
     /// </summary>
